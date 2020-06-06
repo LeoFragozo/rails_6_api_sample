d3e75878c16190147f5d7ac1517b99b6492642f1
Leonardo <lqfragozo@gmail.com>
2020-06-06 19:21:54 -0300

    Fazendo o CRUD da API

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,10 +1,51 @@
 module Api
 	module V1
-    class BooksController < ApplicationController
+		class BooksController < ApplicationController
+
 			def index
 				books = Book.order('created_at ASC');
-				render json: {status: 'SUCCESS', message:'Livros carregados', data:books},status: :ok
+				render json: {status: 'SUCCESS', message:'Livros carregados', data:books},
+				status: :ok
 			end
+
+			def show #está dando erro
+				book = Book.find(params[:id])
+				render json: {status: 'SUCCESS', message:'Livros carregados', data:book},
+				status: :ok
+			end
+
+			def create
+				book = Book.new(book_params)
+				if book.save
+					render json: {status: 'SUCCESS', message:'Livros carregados ', data:book},status: :ok
+				else
+					render json: {status: 'ERROR', message:'Livros não salvos', data:book.errors},status: :unprocessable_entity
+				end
+			end
+
+			def update
+				book = Book.find(params[:id]) #está dando erro
+
+				if book.update_attributes(book_params)
+					render json: {status: 'SUCCESS', message:'Livro atualizado', data:book},
+					status: :ok
+				else
+					render json: {status: 'ERROR', message:'Livro não atualizado', data:book.errors},status: :unprocessable_entity
+				end
+			end
+
+			def destroy
+				book = Book.find(params[:id])
+				book.destroy
+				render json: {status: 'SUCCESS', message:'Livro excluído', data:book},
+				status: :ok
+			end
+
+			private
+			def book_params
+				params.permit(:name, :author, :publication_date)
+			end
+
 		end
 	end
 end
